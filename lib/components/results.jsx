6381f613abf1b787973b5e2bfcef7696184f7c1a
Michael Hirabayashi <mthirabayashi@gmail.com>
2017-02-18 17:13:18 -0800

    recipePuppy api working

--- lib/components/results.jsx
+++ lib/components/results.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import ResultItem from './result_item';
+import axios from 'axios';
+import $ from 'jquery';
 
 class Results extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    // debugger
+    window.$ = $;
+    console.log('results component mounted');
+    // axios.get('https://api.github.com/users/' + 'mthirabayashi')
+    //   .then(function(response){
+    //     console.log(response.data); // ex.: { user: 'Your User'}
+    //     console.log(response.status); // ex.: 200
+    //   });
+    // axios.get('http://www.shareagram.us/api/users/2')
+    //   .then(function(response){
+    //     console.log(response.data); // ex.: { user: 'Your User'}
+    //     console.log(response.status); // ex.: 200
+    //   });
+      const success = function(data) {
+        console.log(data);
+      };
+      const error = function(data) {
+        console.log(data);
+      };
+      // const getRecipes = $.ajax({
+      //   method: 'get',
+      //   url: 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3',
+      //   success
+      // });
+      // url: 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3',
+
+      function logResults(json){
+        console.log(json);
+      }
+
+       $.ajax({
+        url: `http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3`,
+        dataType: 'jsonp',
+        jsonpCallback: 'callback',
+        success
+      });
+
   }
 
   displayAllResults() {
