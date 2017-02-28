ca1234de596714667f6d1173fec1ca88e2c4073b
Michael Hirabayashi <mthirabayashi@gmail.com>
2017-02-27 21:07:56 -0800

    add flexbox

--- lib/components/result_item.jsx
+++ lib/components/result_item.jsx
@@ -4,29 +4,33 @@ class ResultItem extends React.Component {
   constructor(props) {
     super(props);
     // console.log(this.props);
+    this.markVisited = this.markVisited.bind(this);
+    this.state = {
+      opened: false
+    };
   }
+
+  markVisited() {
+    this.setState({opened: true});
+  }
+
   render() {
-    if (this.props.thumbnail.length > 0) {
-      return (
-        <div className='result-item'>
-          <h4 className='result-item-title'>{this.props.title}</h4>
-          <div className='result-item-img-ing'>
-            <img src={this.props.thumbnail} alt="no pic" height="42" width="42"/>
-            <div className='result-ingredients'>
-              <h4>ingredients</h4>
-              <p>{this.props.ingredients}</p>
-            </div>
+    let style = 'not-visited';
+    if (this.state.opened) {
+      style = 'visited';
+    }
+    return (
+      <a href={this.props.url} target="_blank" className={style + " result-item"} onClick={this.markVisited}>
+        <h4 className='result-item-title'>{this.props.title}</h4>
+        <div className='result-item-img-ing'>
+          <img src={this.props.thumbnail} alt="no pic" height="42" width="42"/>
+          <div className='result-ingredients'>
+            <h4>Ingredients</h4>
+            <p>{this.props.ingredients}</p>
           </div>
         </div>
-      );
-    } else {
-      return (
-        <div className='result-item'>
-          <h4>{this.props.title}</h4>
-          <li>{this.props.ingredients}</li>
-        </div>
-      );
-    }
+      </a>
+    );
   }
 }
 
